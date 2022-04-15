e558df71be08d640b240bf61d246c06e952b0a7f
William Meyer <valodier@icloud.com>
2022-04-15 16:10:02 -0400

    Adds rooms table/model

---
+++ db/migrate/20220415200644_create_rooms.rb
@@ -0,0 +1,11 @@
+class CreateRooms < ActiveRecord::Migration[7.0]
+  def change
+    create_table :rooms do |t|
+      t.string :name
+      t.boolean :status, default: true
+      t.string :bgimage
+
+      t.timestamps
+    end
+  end
+end
