aee2a1655a54541e2bf6d9a5d52fd09ed5a093ef
William Meyer <valodier@icloud.com>
2022-04-21 13:50:05 -0400

    Adds room create method

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -6,7 +6,16 @@ class RoomsController < ApplicationController
   end
 
   def create
-    
+    @room = Room.create!(
+      name: params["name"],
+      bgimage: params["bgimage"],
+    )
+
+    if @room.save
+      render :show
+    else
+      render json: {errors: @room.error.full_messages}, status: 422
+    end
   end
 
   def show
